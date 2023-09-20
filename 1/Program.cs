32c580538ed539776494e029b27680f8318ab3d2
BaskakovAnton <8Bazz8@mail.ru>
2023-09-20 20:12:28 +0300

    edit 1/Program.cs

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,8 +1,58 @@
-﻿using System.Xml.Linq;
+﻿using System.Diagnostics;
+/* --------------------------------------------------------------
+using System.Xml.Linq;
 System.Console.Write("Введите число: ");
 int a = int.Parse(Console.ReadLine());
 System.Console.WriteLine(a);
 int sum = a * a;
 System.Console.WriteLine($"Квадрат числа {a} равен = {sum}");
-System.Console.WriteLine("Квадрат числа {0} равен = {1}", a, sum);
+System.Console.WriteLine("Квадрат числа {0} равен = {1}", a, sum); */
 
+/* ---------------------------------------------------------------
+System.Console.Write("Введите первое число: ");
+int num_1 = int.Parse(Console.ReadLine());
+System.Console.Write("Введите второе число: ");
+int num_2 = int.Parse(Console.ReadLine());
+if(num_1 == num_2 * num_2)
+{
+    System.Console.WriteLine("Да");
+}
+else
+{
+    System.Console.WriteLine("Нет");
+} */
+
+System.Console.Write("Введите число дня недели: ");
+int num = int.Parse(Console.ReadLine());
+if (num == 1)
+{
+    Console.Write("Понедельник");
+}
+else if (num == 2)
+{
+    Console.Write("Вторник");
+}
+else if (num == 3)
+{
+    Console.Write("Среда");
+}
+else if (num == 4)
+{
+    Console.Write("Четверг");
+}
+else if (num == 5)
+{
+    Console.Write("Пятница");
+}
+else if (num == 6)
+{
+    Console.Write("Суббота");
+}
+else if (num == 7)
+{
+    Console.Write("Воскресенье");
+}
+else
+{
+    Console.Write("!Неверное число, введите число в диапозоне 1-7!");
+}
