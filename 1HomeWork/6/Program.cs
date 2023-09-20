41f9df8ad02c4e87d17e22739162742ab0dfdcc2
BaskakovAnton <8Bazz8@mail.ru>
2023-09-20 22:32:22 +0300

    edit 1HomeWork/6/Program.cs

--- 1HomeWork/6/Program.cs
+++ 1HomeWork/6/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет */
+
+System.Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+if (num % 2 == 0)
+{
+    System.Console.WriteLine($"Число {num} четное");
+}
+else
+{
+    System.Console.WriteLine($"Число {num} нечетное");
+}
