57e8d56e292b6cdae83f6a897d4e76c508321a29
BaskakovAnton <8Bazz8@mail.ru>
2023-09-20 22:43:06 +0300

    edit 1HomeWork/8/Program.cs

--- 1HomeWork/8/Program.cs
+++ 1HomeWork/8/Program.cs
@@ -1,3 +1,19 @@
 ﻿/* Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 5 -> 2, 4
 8 -> 2, 4, 6, 8 */
+
+System.Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int i = 1;
+while (i <= num)
+{
+    if (i % 2 == 0)
+    {
+        System.Console.Write($"{i}, ");
+        i++;
+    }
+    else
+    {
+        i++;
+    }   
+}
