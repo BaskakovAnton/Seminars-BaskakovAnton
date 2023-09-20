f4614e3653523454a267cc05f9a82f7f13d25016
BaskakovAnton <8Bazz8@mail.ru>
2023-09-20 19:43:17 +0300

    edit 1/program.cs

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Xml.Linq;
+System.Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+System.Console.WriteLine(a);
+int sum = a * a;
+System.Console.WriteLine($"Квадрат числа {a} равен = {sum}");
+System.Console.WriteLine("Квадрат числа {0} равен = {1}", a, sum);
+
