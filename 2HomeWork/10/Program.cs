299bc074118476658688f4f76cc07a1082cd21be
BaskakovAnton <8Bazz8@mail.ru>
2023-09-24 15:47:31 +0300

    add/edit 2HomeWork/10/Program.cs

--- 2HomeWork/10/Program.cs
+++ 2HomeWork/10/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int num = new Random().Next(100, 1000);
+int num_2 = (num % 100) /10;
+System.Console.WriteLine($"{num} -> {num_2}");
