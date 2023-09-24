6a3a9832e54c21c1f578fdce8f347c9cc9bd1d03
BaskakovAnton <8Bazz8@mail.ru>
2023-09-24 14:31:56 +0300

    edit2/Program.cs

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// using System;   Цикл FOR
+// for (int i = 0; i <= 5; i++)
+// {
+//     System.Console.Write($"{i}, "); 
+// }
+
+// System.Console.Write("\b\b ");
+
+// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int num = new Random().Next(10,100);
+int a1 = num /10;
+int a2 = num % 10;
+
+int max = a1;
+
+if(max <a2)
+{
+    max = a2;
+}
+
+System.Console.WriteLine($"Наибольшая цифра числа {num} -> {max} ");
