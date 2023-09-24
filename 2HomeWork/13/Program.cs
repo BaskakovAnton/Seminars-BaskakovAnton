059a9dad150b08092fd67ea01efc69167ed9ac46
BaskakovAnton <8Bazz8@mail.ru>
2023-09-24 18:01:28 +0300

    add/edit 2HomeWork/13/Program.cs

--- 2HomeWork/13/Program.cs
+++ 2HomeWork/13/Program.cs
@@ -2,3 +2,41 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+int GetThirdLeftDigit(int number)
+{
+    int num = Math.Abs(number); // Преобразуем в положительное значение
+
+    if (num < 100)
+    {
+        return -1;
+    }
+    else if (num > 999)
+    {
+        while (num > 999)
+        {
+            num /= 10;
+        }
+        int num3 = num % 10;
+        return num3;
+    }
+    else
+    {
+        int num3 = (num % 100) % 10;
+        return num3;
+    }
+}
+System.Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int thirdLeftDigit = GetThirdLeftDigit(num);
+if (thirdLeftDigit != -1)
+{
+    Console.WriteLine($"Третья цифра слева: {thirdLeftDigit}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры слева нет.");
+}
+
+
+
+
