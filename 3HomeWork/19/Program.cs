a2fa0ca1d12becf4a390221a95864ade6cc61b93
BaskakovAnton <8Bazz8@mail.ru>
2023-09-26 16:44:17 +0300

    edit 3HomeWork/19/Program.cs

--- 3HomeWork/19/Program.cs
+++ 3HomeWork/19/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Answer
+{
+    static bool IsPalindrome(int number)
+    {
+        if (number < 10000 || number > 99999)
+        {
+            System.Console.WriteLine("Число не пятизначное");
+            return false;
+        }
+        int n1 = number % 10;
+        int n2 = number / 10 % 10;
+        int n3 = number / 100 % 10;
+        int n4 = number / 1000 % 10;
+        int n5 = number / 10000 % 10;
+        if (n1 == n5 && n2 == n4)
+            return true;
+        else
+            return false;
+
+    }
+
+    // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args)
+    {
+        int number;
+
+        if (args.Length >= 1)
+        {
+            number = int.Parse(args[0]);
+        }
+        else
+        {
+            // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = 1421288;
+        }
+
+        // Не удаляйте строки ниже
+        bool result = IsPalindrome(number);
+        System.Console.WriteLine($"{result}");
+    }
+}
