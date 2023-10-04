db2094fb2bfab31cac15e14ab3a7f8ed96e62099
BaskakovAnton <8Bazz8@mail.ru>
2023-10-04 19:24:16 +0300

    edit 4HomeWork

--- 4HomeWork/25/Program.cs
+++ 4HomeWork/25/Program.cs
@@ -2,3 +2,18 @@
 // число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+
+Console.WriteLine("Введите число A: ");
+int A = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите степень B: ");
+int B = int.Parse(Console.ReadLine());
+
+int result = 1;
+
+for (int i = 0; i < B; i++)
+{
+    result *= A;
+}
+
+Console.WriteLine($"{A} в степени {B} равно {result}");
