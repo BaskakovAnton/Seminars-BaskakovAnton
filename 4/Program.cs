e5550515274fe018a8a69322c9d286f6c352bb85
BaskakovAnton <8Bazz8@mail.ru>
2023-10-10 19:52:40 +0300

    add docs

--- 4/Program.cs
+++ 4/Program.cs
@@ -89,15 +89,15 @@
 // [1,0,1,1,0,0,1,0,0]
 
 
-void Mass(int col)
-{
-    int[] arry = new int[col];
-    for (int i = 0; i < arry.Length; i++)
-    {
-        arry[i] = new Random().Next(0, 2);
-        System.Console.Write($"{arry[i]} ");
-    }
-}
-System.Console.WriteLine("Введите длину массива: ");
-int n = int.Parse(System.Console.ReadLine());
-Mass(n);
+// void Mass(int col)
+// {
+//     int[] arry = new int[col];
+//     for (int i = 0; i < arry.Length; i++)
+//     {
+//         arry[i] = new Random().Next(0, 2);
+//         System.Console.Write($"{arry[i]} ");
+//     }
+// }
+// System.Console.WriteLine("Введите длину массива: ");
+// int n = int.Parse(System.Console.ReadLine());
+// Mass(n);
--- TEST/Program.cs
+++ TEST/Program.cs
@@ -1,16 +1,1 @@
-﻿System.Console.WriteLine("Введите число: ");
-int number = int.Parse(Console.ReadLine());
-int n1 = number % 10;
-int n2 = number / 10 % 10;
-int n3 = number / 100 % 10;
-int n4 = number / 1000 % 10;
-int n5 = number / 10000 % 10;
-System.Console.WriteLine(n1);
-System.Console.WriteLine(n2);
-System.Console.WriteLine(n3);
-System.Console.WriteLine(n4);
-System.Console.WriteLine(n5);
-if (n1 == n5 && n2 == n4)
-    return true;
-else if(n1 != n5 || n2 != n4)
-    return false;
+﻿
